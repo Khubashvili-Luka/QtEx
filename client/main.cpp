1eda85b53381f0ee27d18f42b56a4c13f0204d05
unknown <bahp2003@gmail.com>
2024-10-17 02:42:41 +0300

    cl

---
+++ client/main.cpp
@@ -0,0 +1,16 @@
+#include <AuthWindow.h>
+#include <mainwindow.h>
+#include <teacherwindow.h>
+#include <clientfunctions.h>
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    AuthWindow authw;
+    MainWindow mainw;
+
+    load_setting("login") == "-1" ?authw.show() : mainw.show();
+
+    return a.exec();
+}
---
+++ client/mainwindow.h
@@ -0,0 +1,50 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <clientfunctions.h>
+#include <QApplication>
+#include <QProcess>
+#include <taskform.h>
+
+namespace Ui {
+class MainWindow;
+}
+
+/*!
+ * \brief Класс окна учащегося
+ */
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    /*!
+     * \brief Конструктор окна
+     * \param parent
+     */
+    explicit MainWindow(QWidget *parent = nullptr);
+    /*!
+     * \brief Деструктор окна
+     */
+    ~MainWindow();
+    /*!
+     * \brief Функция обновления статистики
+     * \return
+     */
+
+signals:
+    void openWindow();
+
+private slots:
+    void on_task1_clicked();
+    void on_task2_clicked();
+    void on_task3_clicked();
+    void on_task4_clicked();
+    void on_account_exit_triggered();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
